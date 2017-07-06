ea2f493984ef9eafffba6e3d2763aead91b01190
Bradley Sliz <bradsliz@gmail.com>
2017-07-06 12:01:59 -0500

    Create setup.py

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='swagbag',
+      version='0.1',
+      description='Access to themed color pallettes and images',
+      author='Bradley Sliz',
+      author_email='bradsliz@gmail.com',
+      url='https://github.com/slizb/swag-bag',
+      packages=['swagbag'],
+     )
