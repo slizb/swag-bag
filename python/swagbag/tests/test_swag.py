7599adeeab88b9e9c7021b763b0818c1cde56666
Brad Sliz <bradsliz@gmail.com>
2017-08-03 14:25:49 -0500

    adding simple tests for teamLogos class

--- python/swagbag/tests/test_swag.py
+++ python/swagbag/tests/test_swag.py
@@ -8,12 +8,24 @@ print(my_wd)
 sys.path.append(my_wd + '/python')
 
 from swagbag import swag
+from PIL.PngImagePlugin import PngImageFile
+
+
+class TestTeamLogos(unittest.TestCase):
+    def test_fake_team_doesnt_work(self):
+        img = swag.TeamLogos().get_logo('Decatur Fakers')
+        self.assertNotIsInstance(img, PngImageFile)
+
+    def test_real_team_works(self):
+        img = swag.TeamLogos().get_logo('Chicago Bulls')
+        self.assertIsInstance(img, PngImageFile)
+
 
 class TestUtil(unittest.TestCase):
     def test_read_svg_from_web(self):
         homer_svg = 'http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg'
         img = swag.Util().read_svg_from_web(homer_svg, 1)
-        self.assertIsInstance(img, swag.PIL.PngImagePlugin.PngImageFile)
+        self.assertIsInstance(img, PngImageFile)
 
 if __name__ == '__main__':
     unittest.main()
